ef49a2f4ea94a6e0e8d0ee4eb5704b4b8785d363
xorbadiaclabz <xorbadiac@localhost.localdomain>
2021-12-06 18:36:19 -0400

    Add Main Data Entry and Record Information Section

--- abq_labs.py
+++ abq_labs.py
@@ -7,3 +7,35 @@ from datetime import datetime
 from pathlib import Path
 import csv
 
+variables = dict()
+records_saved = 0
+
+root = tk.Tk()
+root.title('ABQ Data Entry Application')
+# Prevent menus to stay on the left if root window's expanded
+root.columnconfigure(0, weight = 1)
+
+ttk.Label(
+    root, text ="ABQ Data Entry Application",
+    font = ("TkDefaultFont", 16)
+    ).grid()
+
+# Main Data Entry Form (drf)
+
+drf = ttk.Frame(root)
+drf.grid(padx=10, sticky = (tk.E + tk.W))
+drf.columnconfigure(0, weight = 1)
+
+# Record information Section
+
+rec_info = ttk.LabelFrame(drf, text = 'Record Information')
+rec_info.grid(sticky = (tk.W + tk.E))
+for i in range(3):
+    rec_info.columnconfigure(i, weight = 1)
+
+variables['Date'] = tk.StringVar()
+ttk.Label(rec_info, text = 'Date').grid(row = 0, column = 0)
+ttk.Entry(
+    rec_info,
+    textvariable = variables['Date']
+    ).grid(row = 1, column = 0, sticky = (tk.W + tk.E))
