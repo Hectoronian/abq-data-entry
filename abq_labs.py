a6ad52fdd5a8175dafc6feabd5cba4bcd3ad4bdf
xorbadiaclabz <xorbadiac@localhost.localdomain>
2021-12-08 21:22:07 -0400

    Add lab input, plot, and seed samples, and environment data section (only the Humidity)

--- abq_labs.py
+++ abq_labs.py
@@ -39,3 +39,53 @@ ttk.Entry(
     rec_info,
     textvariable = variables['Date']
     ).grid(row = 1, column = 0, sticky = (tk.W + tk.E))
+
+time_values = ['8:00', '12:00', '16:00', '20:00']
+variables['Time'] = tk.StringVar()
+ttk.Label(rec_info, text = 'Time').grid(row = 0, column = 1)
+ttk.Combobox(
+    rec_info,
+    textvariable = variables['Time'],
+    values = time_values).grid(row = 1, column = 1, sticky = (tk.W + tk.E))
+
+# Second row lab input
+
+variables['Lab'] = tk.StringVar()
+ttk.Label(rec_info, text='Lab').grid(row = 2, column = 0)
+labframe = ttk.Frame(rec_info)
+for lab in ('A', 'B', 'C'):
+    ttk.Radiobutton(
+        labframe, value = lab, text = lab, variable = variables['Lab']
+        ).pack(side = tk.LEFT, expand = True)
+labframe.grid(row = 3, column = 0, sticky = (tk.W + tk.E))
+
+# Plot and seed samples
+
+variables['Plot'] = tk.IntVar()
+ttk.Label(rec_info, text = 'Plot').grid(row = 2, column = 1)
+ttk.Combobox(
+    rec_info,
+    textvariable = variables['Plot'],
+    values = list(range(1, 21))
+    ).grid(row = 3, column = 1, sticky = (tk.W + tk.E))
+
+variables['Seed Sample'] = tk.StringVar()
+ttk.Label(rec_info, text = 'Seed Sample').grid(row = 2, column = 2)
+ttk.Entry(
+    rec_info,
+    textvariable = variables['Seed Sample']
+    ).grid(row = 3, column = 2, sticky = (tk.W + tk.E))
+
+# Environment Data Section
+
+env_info = ttk.LabelFrame(drf, text = "Environment Data")
+env_info.grid(sticky = (tk.W + tk.E))
+for i in range(3):
+    env_info.columnconfigure(i, weight = 1)
+
+variables['Humidity'] = tk.DoubleVar()
+ttk.Label(env_info, text = "Humidity (g/m3)").grid(row = 0, column = 0)
+ttk.Spinbox(
+    env_info, textvariable = variables['Humidity'],
+    from_ = 0.5, to = 52.0, increment = 0.01
+    ).grid(row = 1, column = 0, sticky = (tk.W + tk.E))
